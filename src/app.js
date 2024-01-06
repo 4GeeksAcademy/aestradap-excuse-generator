235d6b5fa6edcf8baff29f214ebb282043c0e28d
aestradap <55397157+aestradap@users.noreply.github.com>
2024-01-06 02:28:02 +0000

    excuses exercise complete principal functions (rIndex / to get a random #, concatExcuse / to assamble the excuse, onLoad / to set the values in the doc )

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import { who, action, what, when } from "./text";
+import { images } from "./images";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  let dExcuse = "The dog crushed my homework when I finished";
+
+  const rIndex = () => Math.floor(Math.random() * 4 + 1) - 1;
+
+  const concatExcuse = () => {
+    dExcuse = `${who[rIndex()]} ${action[rIndex()]} ${what[rIndex()]} 
+    ${when[rIndex()]}`;
+    return dExcuse;
+  };
+
+  const onload = dExcuse => {
+    // document.getElementById("excuse").innerHTML = concatExcuse(dExcuse);
+    // document.getElementById("image").setAttribute("src", images[rIndex()]);
+    document.querySelector("#excuse").textContent = concatExcuse(dExcuse);
+    document.querySelector("#image").setAttribute("src", images[rIndex()]);
+  };
+
+  onload(dExcuse);
 };
---
+++ src/images.js
@@ -0,0 +1,6 @@
+export let images = [
+  "src/assets/img/3.jpg",
+  "src/assets/img/2.jpg",
+  "src/assets/img/1.jpg",
+  "src/assets/img/4.jpg"
+];
